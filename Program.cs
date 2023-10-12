f5db0f4a1eb062fd820384d8e8fa54ed48bcc0d9
Dungeater1337 <111962656+Dungeater1337@users.noreply.github.com>
2023-10-12 16:20:46 +0200

    KJlar klar
    kalkr

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿const string input = "[()]{}{[()()]}";
+Stack<char> stack = new Stack<char>(); 
+bool balanced = true; 
+
+foreach(char c in input)
+{
+    if(c == '[' || c == '{' ||  c == '(')
+    {
+        stack.Push(c); 
+    }
+
+    else
+    {
+        char start = stack.Pop();
+        if(
+            (c == ')' && start != '(') ||
+            (c == ']' && start != '[') ||
+            (c == '}' && start != '{') 
+        )
+        {
+            balanced = false;
+            break;
+        }
+    }
+}
+
+if(balanced)
+{
+    Console.WriteLine("String is balanced");
+}
+
+else{
+    Console.WriteLine("String is not balanced");
+}
